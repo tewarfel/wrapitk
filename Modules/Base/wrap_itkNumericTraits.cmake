61ca014ac9c6582b80a717c08ba05154c6dba98a
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-01-19 15:52:45 +0000

    more numeric traits available

--- Modules/Base/wrap_itkNumericTraits.cmake
+++ Modules/Base/wrap_itkNumericTraits.cmake
@@ -2,9 +2,12 @@
 SET(WRAPPER_AUTO_INCLUDE_HEADERS OFF)
 WRAP_INCLUDE("itkNumericTraits.h")
 WRAP_INCLUDE("itkNumericTraitsRGBPixel.h")
+WRAP_INCLUDE("itkNumericTraitsRGBAPixel.h")
 WRAP_INCLUDE("itkNumericTraitsTensorPixel.h")
 WRAP_INCLUDE("itkNumericTraitsVariableLengthVectorPixel.h")
+WRAP_INCLUDE("itkNumericTraitsFixedArrayPixel.h")
 WRAP_INCLUDE("itkNumericTraitsVectorPixel.h")
+WRAP_INCLUDE("itkNumericTraitsCovariantVectorPixel.h")
 
 # the superclass
 WRAP_CLASS(vcl_numeric_limits FORCE_INSTANTIATE)
@@ -12,20 +15,6 @@ WRAP_CLASS(vcl_numeric_limits FORCE_INSTANTIATE)
   FOREACH(t UC US UI UL SC SS SI SL F D LD B)
     WRAP_TEMPLATE("${ITKM_${t}}" "${ITKT_${t}}")
   ENDFOREACH(t)
-  
-  # the ITK types
-  # TODO: Get why build fail with RBG and vectors types and wrap them 
-#  FOREACH(t ${WRAP_ITK_COMPLEX_REAL})   # ${WRAP_ITK_RGB} 
-#    WRAP_TEMPLATE("${ITKM_${t}}" "${ITKT_${t}}")
-#  ENDFOREACH(t)
-  
-#  FOREACH(d ${WRAP_ITK_DIMS})
-#    FOREACH(t ${WRAP_ITK_VECTOR_REAL} ${WRAP_ITK_COV_VECTOR_REAL})
-#      WRAP_TEMPLATE("${ITKM_${t}${d}}" "${ITKT_${t}${d}}")
-#    ENDFOREACH(t)
-#  ENDFOREACH(d)
-  
-  # TODO: the VariableLengthVectorPixel, which is not defined in WraITKTypes.cmake
 END_WRAP_CLASS()
 
 
@@ -35,17 +24,65 @@ WRAP_CLASS("itk::NumericTraits")
     WRAP_TEMPLATE("${ITKM_${t}}" "${ITKT_${t}}")
   ENDFOREACH(t)
   
-  # the ITK types
-  # TODO: Get why build fail with RBG and vectors types and wrap them 
-#  FOREACH(t ${WRAP_ITK_COMPLEX_REAL})   # ${WRAP_ITK_RGB} 
+#  FOREACH(t ${WRAP_ITK_COMPLEX_REAL})
 #    WRAP_TEMPLATE("${ITKM_${t}}" "${ITKT_${t}}")
 #  ENDFOREACH(t)
+
+  # the ITK types
   
-#  FOREACH(d ${WRAP_ITK_DIMS})
-#    FOREACH(t ${WRAP_ITK_VECTOR_REAL} ${WRAP_ITK_COV_VECTOR_REAL})
-#      WRAP_TEMPLATE("${ITKM_${t}${d}}" "${ITKT_${t}${d}}")
-#    ENDFOREACH(t)
-#  ENDFOREACH(d)
+  # rgb, rgba
+  FOREACH(t ${WRAP_ITK_RGB})
+    WRAP_TEMPLATE("${ITKM_${t}}" "${ITKT_${t}}")
+  ENDFOREACH(t)
+
+  # covariant vector
+  FOREACH(d ${WRAP_ITK_DIMS})
+    FOREACH(t ${WRAP_ITK_COV_VECTOR_REAL})
+      WRAP_TEMPLATE("${ITKM_${t}${d}}" "${ITKT_${t}${d}}")
+    ENDFOREACH(t)
+  ENDFOREACH(d)
   
-  # TODO: the VariableLengthVectorPixel, which is not defined in WraITKTypes.cmake
+  # vector, as in WrapITKTypes.cmake
+  UNIQUE(vector_sizes "1;${WRAP_ITK_DIMS};6")
+  UNIQUE(vector_types "UC;F;D;${WRAP_ITK_SCALAR}")
+  FOREACH(d ${vector_sizes})
+    FOREACH(t ${vector_types})
+      ADD_TEMPLATE("${ITKM_V${t}${d}}" "${ITKT_V${t}${d}}")
+    ENDFOREACH(t)
+  ENDFOREACH(d)
+
+  # fixed array, as in WrapITKTypes.cmake
+  SET(dims ${WRAP_ITK_DIMS})
+  FOREACH(d ${WRAP_ITK_DIMS})
+    MATH(EXPR d2 "${d} * 2")
+    # for itk::SymmetricSecondRankTensor
+    MATH(EXPR d3 "${d} * (${d} + 1) / 2")
+    SET(dims ${dims} ${d2} ${d3})
+  ENDFOREACH(d)
+  UNIQUE(array_sizes "${dims};1;3;4;6")
+  # make sure that 1-D FixedArrays are wrapped. Also wrap for each selected
+  # image dimension.
+  # 3-D FixedArrays are required as superclass of rgb pixels
+  # TODO: Do we need fixed arrays for all of these types? For just the selected
+  # pixel types plus some few basic cases? Or just for a basic set of types?
+  FOREACH(d ${array_sizes})
+    ADD_TEMPLATE("${ITKM_FAD${d}}"  "${ITKT_FAD${d}}")
+    ADD_TEMPLATE("${ITKM_FAF${d}}"  "${ITKT_FAF${d}}")
+    ADD_TEMPLATE("${ITKM_FAUL${d}}" "${ITKT_FAUL${d}}")
+    ADD_TEMPLATE("${ITKM_FAUS${d}}" "${ITKT_FAUS${d}}")
+    ADD_TEMPLATE("${ITKM_FAUC${d}}" "${ITKT_FAUC${d}}")
+    ADD_TEMPLATE("${ITKM_FAUI${d}}" "${ITKT_FAUI${d}}")
+    ADD_TEMPLATE("${ITKM_FASL${d}}" "${ITKT_FASL${d}}")
+    ADD_TEMPLATE("${ITKM_FASS${d}}" "${ITKT_FASS${d}}")
+    ADD_TEMPLATE("${ITKM_FASC${d}}" "${ITKT_FASC${d}}")
+    # this one is not defined in itkNumerictTraitsFixedArrayPixel.h
+    # ADD_TEMPLATE("${ITKM_FAB${d}}"  "${ITKT_FAB${d}}")
+  ENDFOREACH(d)
+
+  # variable length vector, as in WrapITKTypes.cmake
+  UNIQUE(wrap_image_types "${WRAP_ITK_SCALAR};UC")
+  FOREACH(type ${wrap_image_types})
+    ADD_TEMPLATE("${ITKM_VLV${type}}"  "${ITKT_VLV${type}}")
+  ENDFOREACH(type)
+
 END_WRAP_CLASS()
