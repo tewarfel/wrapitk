e6811f45ae54d0e9cabdbe94a4d1f91873aaef02
Tonddast <Tonddast@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-01-21 17:04:36 +0000

    ITK specific Doxygen to SWIG python script.

---
+++ trunk/Languages/Doc/itk_doxy2swig.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Doxygen XML to SWIG docstring converter for ITK classes.
+
+Usage:
+
+  itk_doxy2swig.py [input directory name] [output swing interface file name]
+
+"""
+
+import sys
+import glob
+from doxy2swig import *
+
+def d2s_dir(in_dir_name, out_swig_i):
+	fpattern = in_dir_name + "/class*.xml"
+	xml_file_names = glob.glob(fpattern)
+	open(out_swig_i, 'w')
+	for xfn in xml_file_names:
+		print("-- Doxygen to SWIG:" + xfn)
+		d2s = Doxy2SWIG(xfn)
+		d2s.generate()
+		d2s.write(out_swig_i, 'a+')
+
+def main(in_dir_name, out_swig_i):
+	d2s_dir(in_dir_name, out_swig_i)
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		print __doc__
+		sys.exit(1)
+	main(sys.argv[1], sys.argv[2])
