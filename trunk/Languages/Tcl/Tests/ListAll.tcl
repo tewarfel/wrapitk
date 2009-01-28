72259b3c2121d6d4e7e455c0297829e9e3004478
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-01-28 14:16:58 +0000

    test fixes

--- trunk/Languages/Tcl/Tests/ListAll.tcl
+++ trunk/Languages/Tcl/Tests/ListAll.tcl
@@ -6,7 +6,7 @@ if {$argc == 0} {
     set fileid [open [lindex $argv 0] w]
 }
 
-set b [itkStringStream]
+# set b [itkStringStream]
 set allClasses [info command itk*]
 
 foreach class $allClasses {
