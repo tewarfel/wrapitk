7b97704104b892c349f34a0fe9e7a7fe4ce2f00e
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-01-30 10:25:45 +0000

    ooops, wrong name\!

--- trunk/Languages/Doc/itk_doxy2swig.py
+++ trunk/Languages/Doc/itk_doxy2swig.py
@@ -97,7 +97,7 @@ def d2s_dir(in_dir_name, out_swig_i):
           for swig_name in ls[2:]:
             output.write(tpl.replace("@[{(]})@", swig_name))
       else:
-        print >> sys.argv, "Warning: %s does not exist. Ignore it." % xfn
+        print >> sys.stderr, "Warning: %s does not exist. Ignore it." % xfn
   f = open(out_swig_i, 'w')
   f.write(output.getvalue())
   f.close()
