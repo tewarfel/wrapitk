c702b8b46c016b784210ed2388d9ade50d711df9
Tonddast <Tonddast@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-02-13 13:00:32 +0000

    ItkDolfin updates - resolving conflicts.

---
+++ ExternalProjects/ItkDolfin/tests/test-01.py
@@ -0,0 +1,33 @@
+from dolfin import *
+import viper
+import itk
+import time
+
+dim = 2
+inType = itk.Image[itk.D, dim]
+
+reader = itk.ImageFileReader[inType].New(FileName="/media/USB-WD-1/data/tests/image_0010.bmp")
+#reader = itk.ImageFileReader[inType].New(FileName="/path/to/file.bmp")
+reader.Update()
+
+t1 = time.time()
+
+itk2dolfin = itk.ImageToDolfinFunction[inType]
+#itk2dolfin.SetInput(itk2dolfin, reader.GetOutput())
+#itk2dolfin.Update()
+#f = itk2dolfin.GetOutput()
+f = itk2dolfin.Convert(reader.GetOutput())
+
+#imfun = itk.DolfinImageFunction[inType]
+
+t2 = time.time()
+#print '%i * %i = %i pixels took %0.3f ms transfering from itk to dolfin through numpy'% (shape[0], shape[1], shape[0]*shape[1], (t2-t1)*1e3)
+#print 'That is %f us per pixel.'% ((t2-t1) * 1e6 / (shape[0]*shape[1]))
+
+plot(f) # can viper plot 2d images better than this?
+interactive()
+
+#v = viper.Viper(f)
+#v.interactive()
+
+
--- ExternalProjects/ItkDolfin/wrapping/wrap_itkImageToDolfinFunction.cmake
+++ ExternalProjects/ItkDolfin/wrapping/wrap_itkImageToDolfinFunction.cmake
@@ -1,4 +1,9 @@
 WRAP_CLASS("itk::ImageToDolfinFunction")
-  UNIQUE(types "${WRAP_ITK_SCALAR};UC")
+  UNIQUE(types "${WRAP_ITK_SCALAR};D")
+  WRAP_IMAGE_FILTER("${types}" 1)
+END_WRAP_CLASS()
+
+WRAP_CLASS("itk::DolfinImageFunction")
+  UNIQUE(types "${WRAP_ITK_SCALAR};D")
   WRAP_IMAGE_FILTER("${types}" 1)
 END_WRAP_CLASS()
