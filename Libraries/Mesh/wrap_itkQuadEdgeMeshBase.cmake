d97632b1230e927769146daad2c8ed24cbdf6c9f
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-07-31 20:58:25 +0000

    a few fixes for QuadEdgeMesh code

--- Libraries/Mesh/wrap_itkQuadEdgeMeshBase.cmake
+++ Libraries/Mesh/wrap_itkQuadEdgeMeshBase.cmake
@@ -7,9 +7,7 @@ WRAP_CLASS("itk::MapContainer" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
     WRAP_TEMPLATE("ULQEMPF${d}" "unsigned long, itk::QuadEdgeMeshPoint< float, ${d} >")
 #     WRAP_TEMPLATE("MD${d}QBAIUL" "itk::Mesh<double, ${d}u, itk::QuadEdgeMeshTraits<double, ${d}, bool, bool, float, float> >::BoundaryAssignmentIdentifier, unsigned long")
-
-# commented because the superclass make swig produce some invalid code.
-#    WRAP_TEMPLATE("ULCIDQEMCTI${d}" "unsigned long, itk::CellInterface< double, itk::QuadEdgeMeshCellTraitsInfo< ${d} > >*")
+    WRAP_TEMPLATE("ULCIDQEMCTI${d}" "unsigned long, itk::CellInterface< double, itk::QuadEdgeMeshCellTraitsInfo< ${d} > >*")
   ENDFOREACH(d)
 END_WRAP_CLASS()
 
