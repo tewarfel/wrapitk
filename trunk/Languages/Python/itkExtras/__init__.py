fe0d41bb124c9c5b76cc166748bf90821801f3ef
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2009-11-20 09:47:12 +0000

    add functions to make the manipulation of label map attributes easier

--- trunk/Languages/Python/itkExtras/__init__.py
+++ trunk/Languages/Python/itkExtras/__init__.py
@@ -820,6 +820,75 @@ def down_cast(obj):
   else:
     return t.cast(obj)
     
+def attribute_list( i, name ):
+  """Returns a list of the specified attributes for the objects in the image.
+  
+  i: the input LabelImage
+  name: the attribute name
+  """
+  import itk
+  i = itk.output(i)
+  relabel = itk.StatisticsRelabelLabelMapFilter[i].New(i, Attribute=name, ReverseOrdering=True, InPlace=False)
+  relabel.UpdateLargestPossibleRegion()
+  r = relabel.GetOutput()
+  l = []
+  for i in range(1, r.GetNumberOfLabelObjects()+1):
+    l.append( r.GetLabelObject(i).__getattribute__("Get"+name)() )
+  return l
+
+
+def attributes_list( i, names ):
+  """Returns a list of the specified attributes for the objects in the image.
+  
+  i: the input LabelImage
+  name: the attribute name
+  """
+  import itk
+  i = itk.output(i)
+  relabel = itk.StatisticsRelabelLabelMapFilter[i].New(i, Attribute=names[0], ReverseOrdering=True, InPlace=False)
+  relabel.UpdateLargestPossibleRegion()
+  r = relabel.GetOutput()
+  l = []
+  for i in range(1, r.GetNumberOfLabelObjects()+1):
+    attrs = []
+    for name in names :
+      attrs.append( r.GetLabelObject(i).__getattribute__("Get"+name)() )
+    l.append( tuple( attrs ) )
+  return l
+
+
+def attribute_dict( i, name ):
+  """Returns a dict with the attribute values in keys and a list of the corresponding objects in value
+  
+  i: the input LabelImage
+  name: the name of the attribute
+  """
+  import itk
+  i = itk.output(i)
+  relabel = itk.StatisticsRelabelLabelMapFilter[i].New(i, Attribute=name, ReverseOrdering=True, InPlace=False)
+  relabel.UpdateLargestPossibleRegion()
+  r = relabel.GetOutput()
+  d = {}
+  for i in range(1, r.GetNumberOfLabelObjects()+1):
+    lo = r.GetLabelObject(i)
+    v = lo.__getattribute__("Get"+name)()
+    l = d.get( v, [] )
+    l.append( lo )
+    d[v] = l
+  return d
+
+
+def number_of_objects( i ):
+  """Returns the number of objets in the image.
+  
+  i: the input LabelImage
+  """
+  import itk
+  i.UpdateLargestPossibleRegion()
+  i =  itk.output(i)
+  return i.GetNumberOfLabelObjects()
+
+
 # now loads the other modules we may found in the same directory
 import os.path, sys
 directory = os.path.dirname(__file__)
