f695f7c422534f3f977d242a6371c9a1260db08f
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2010-01-22 16:50:39 +0000

    use only dim=3 for DTITube classes

--- Libraries/SpatialObject/wrap_itkDTITubeSpatialObjectPoint.cmake
+++ Libraries/SpatialObject/wrap_itkDTITubeSpatialObjectPoint.cmake
@@ -1,5 +1,12 @@
 WRAP_CLASS("itk::DTITubeSpatialObjectPoint")
-  FOREACH(d ${WRAP_ITK_DIMS})
-    WRAP_TEMPLATE("${d}" "${d}")
-  ENDFOREACH(d)
+#  FOREACH(d ${WRAP_ITK_DIMS})
+#    WRAP_TEMPLATE("${d}" "${d}")
+#  ENDFOREACH(d)
+
+  # seems to be usable only with dim=3       
+  FILTER_DIMS(d 3)                                
+  IF(d)
+    WRAP_TEMPLATE(3 3)
+  ENDIF(3)
+
 END_WRAP_CLASS()
