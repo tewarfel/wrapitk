6919ff96c2b2dcd655b2b07419177f1aecf74519
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2010-01-30 21:33:00 +0000

    add HessianToObjectnessMeasureImageFilter

---
+++ Libraries/Filtering/wrap_itkHessianToObjectnessMeasureImageFilter.cmake
@@ -0,0 +1,7 @@
+WRAP_CLASS("itk::HessianToObjectnessMeasureImageFilter" POINTER)
+  FOREACH(d ${WRAP_ITK_DIMS})
+    FOREACH(t ${WRAP_ITK_SCALAR})
+      WRAP_TEMPLATE("${ITKM_ISSRTD${d}${d}}${ITKM_I${t}${d}}"  "${ITKT_ISSRTD${d}${d}}, ${ITKT_I${t}${d}}")
+    ENDFOREACH(t)
+  ENDFOREACH(d)
+END_WRAP_CLASS()
