14030062b30d04a96dc8886b35bc48951a2d38af
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2010-02-02 18:10:33 +0000

    add KdTree and a the required classes

---
+++ trunk/Libraries/Numerics/wrap_itkMembershipFunctionBase.cmake
@@ -0,0 +1,5 @@
+WRAP_CLASS("itk::Statistics::MembershipFunctionBase" POINTER)
+  FOREACH(d ${WRAP_ITK_DIMS})
+    WRAP_TEMPLATE("${ITKM_VF${d}}"  "${ITKT_VF${d}}")
+  ENDFOREACH(d)
+END_WRAP_CLASS()
