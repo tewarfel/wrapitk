cb5c4b8378d98f33cd4b7ffbecab3632fa9acaef
gaetan.lehmann <gaetan.lehmann@7d95c506-104e-0410-b500-6ba8341cbcc0>
2010-08-04 14:20:46 +0000

    remove now useless specific wrapper for msvc

--- trunk/Libraries/VXLNumerics/wrap_vcl_complex.cmake
+++ trunk/Libraries/VXLNumerics/wrap_vcl_complex.cmake
@@ -5,12 +5,3 @@ WRAP_CLASS("vcl_complex" FORCE_INSTANTIATE)
   ENDFOREACH(t)
 
 END_WRAP_CLASS()
-
-IF(WIN32)
-  SET(WRAPPER_AUTO_INCLUDE_HEADERS OFF)
-  WRAP_CLASS("std::_Complex_base" FORCE_INSTANTIATE)
-    WRAP_TEMPLATE("F" "float, _C_float_complex")
-    WRAP_TEMPLATE("D" "double, _C_double_complex")
-    WRAP_TEMPLATE("LD" "long double, _C_ldouble_complex")
-  END_WRAP_CLASS()
-ENDIF(WIN32)
